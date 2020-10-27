2a0bc9e5ceb4991ebe6f163714c2f6b2b96c2954
Yann Pauly <yann.pauly@ovhcloud.com>
2020-10-27 02:42:32 +0100

    optimization exercice

--- employees.sql
+++ employees.sql
@@ -41,8 +41,8 @@ DROP TABLE IF EXISTS dept_emp,
 CREATE TABLE employees (
     emp_no      INT             NOT NULL,
     birth_date  DATE            NOT NULL,
-    first_name  VARCHAR(14)     NOT NULL,
-    last_name   VARCHAR(16)     NOT NULL,
+    first_name  VARCHAR(250)     NOT NULL,
+    last_name   VARCHAR(250)     NOT NULL,
     gender      ENUM ('M','F')  NOT NULL,    
     hire_date   DATE            NOT NULL,
     PRIMARY KEY (emp_no)
@@ -50,9 +50,8 @@ CREATE TABLE employees (
 
 CREATE TABLE departments (
     dept_no     CHAR(4)         NOT NULL,
-    dept_name   VARCHAR(40)     NOT NULL,
-    PRIMARY KEY (dept_no),
-    UNIQUE  KEY (dept_name)
+    dept_name   VARCHAR(250)     NOT NULL,
+    PRIMARY KEY (dept_no)
 );
 
 CREATE TABLE dept_manager (
@@ -60,8 +59,6 @@ CREATE TABLE dept_manager (
    dept_no      CHAR(4)         NOT NULL,
    from_date    DATE            NOT NULL,
    to_date      DATE            NOT NULL,
-   FOREIGN KEY (emp_no)  REFERENCES employees (emp_no)    ON DELETE CASCADE,
-   FOREIGN KEY (dept_no) REFERENCES departments (dept_no) ON DELETE CASCADE,
    PRIMARY KEY (emp_no,dept_no)
 ); 
 
@@ -70,17 +67,14 @@ CREATE TABLE dept_emp (
     dept_no     CHAR(4)         NOT NULL,
     from_date   DATE            NOT NULL,
     to_date     DATE            NOT NULL,
-    FOREIGN KEY (emp_no)  REFERENCES employees   (emp_no)  ON DELETE CASCADE,
-    FOREIGN KEY (dept_no) REFERENCES departments (dept_no) ON DELETE CASCADE,
     PRIMARY KEY (emp_no,dept_no)
 );
 
 CREATE TABLE titles (
     emp_no      INT             NOT NULL,
-    title       VARCHAR(50)     NOT NULL,
+    title       VARCHAR(250)     NOT NULL,
     from_date   DATE            NOT NULL,
     to_date     DATE,
-    FOREIGN KEY (emp_no) REFERENCES employees (emp_no) ON DELETE CASCADE,
     PRIMARY KEY (emp_no,title, from_date)
 ) 
 ; 
@@ -90,7 +84,6 @@ CREATE TABLE salaries (
     salary      INT             NOT NULL,
     from_date   DATE            NOT NULL,
     to_date     DATE            NOT NULL,
-    FOREIGN KEY (emp_no) REFERENCES employees (emp_no) ON DELETE CASCADE,
     PRIMARY KEY (emp_no, from_date)
 ) 
 ; 
